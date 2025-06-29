242ba75761fd7d581ec95dd9489095105f137fac
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-06-29 18:01:17 +0600

    changed header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>Header Section</div>
+        <div className='w-full bg-black text-white py-3 m-auto px-0 text-center'>
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%! <a className="text-white border-b-1" href="">ShopNow</a>
+        </div>
     )
 }
 
