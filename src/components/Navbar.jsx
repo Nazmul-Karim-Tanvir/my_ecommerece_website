3dd8a0495d7a15c8308335c4289c0a18b419696f
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-06-30 13:35:31 +0600

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Heart, ShoppingCart, } from 'lucide-react'; // using lucide-react icons
+import { Heart, ShoppingCart, Search } from 'lucide-react'; // using lucide-react icons
 
 const Navbar = () => {
     return (
-        <nav className="bg-white py-4 px-8">
+        <nav className="bg-white pt-8 pb-4 px-8 border-b border-gray-300">
             <div className="container mx-auto flex items-center justify-between">
 
                 {/* Brand */}
-                <div className="text-xl font-bold text-gray-800">
+                <div className="text-xl font-bold text-gray-900">
                     Exclusive
                 </div>
 
                 {/* Nav Links */}
                 <ul className="flex gap-8 text-gray-700 font-sans font-medium">
-                    <li><Link to="/" className="hover:text-blue-500">Home</Link></li>
-                    <li><Link to="/about" className="hover:text-blue-500">About</Link></li>i
-                    <li><Link to="/contact" className="hover:text-blue-500">Contact</Link></li>
-                    <li><Link to="/signup" className="hover:text-blue-500">Sign Up</Link></li>
+                    <li><Link to="/" className="hover:underline hover:underline-offset-4">Home</Link></li>
+                    <li><Link to="/about" className="hover:underline hover:underline-offset-4">About</Link></li>
+                    <li><Link to="/contact" className="hover:underline hover:underline-offset-4">Contact</Link></li>
+                    <li><Link to="/signup" className="hover:underline hover:underline-offset-4">Sign Up</Link></li>
                 </ul>
 
                 <div className="ml-6 flex items-center">
-                    {/* Search box */}
-                    <input
-                        type="text"
-                        placeholder="What are you Looking for?"
-                        className="border border-gray-300 px-3 py-1 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
-                    />
+                    <div className='flex border border-gray-300 px-3 py-1 rounded-md w-60'>
+                        {/* Search box */}
+                        <input
+                            type="text"
+                            placeholder="What are you Looking for?"
+                            className="flex-grow outline-none text-sm"
+                        />
+                        <Search className='ml-3 w-5 h-5' />
+                    </div>
+
 
                     {/* Icons */}
                     <div className="flex items-center gap-4 text-gray-700 ml-6">
