c757c491ce10903a6f8d7df2eeb42b578df5d91b
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-07-14 17:48:02 +0600

    added some animation

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,3 +1,7 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 import SectionHero from './SectionHero';
 import SectionToday from './SectionToday';
 import SectionCategories from './SectionCategories';
@@ -8,18 +12,25 @@ import SectionNewArrival from './SectionNewArrival';
 import Services from '../../components/Services';
 
 const HomePage = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 800, // animation duration
+            once: true, // animation only once
+        });
+    }, []);
+
     return (
-        <div className=''>
-            <SectionHero />
-            <SectionToday />
-            <SectionCategories />
-            <SectionBestSelling />
-            <SectionExclusiveOffer />
-            <SectionExploreProducts />
-            <SectionNewArrival />
-            <Services />
+        <div>
+            <div data-aos="fade-up"><SectionHero /></div>
+            <div data-aos="fade-up"><SectionToday /></div>
+            <div data-aos="fade-up"><SectionCategories /></div>
+            <div data-aos="fade-up"><SectionBestSelling /></div>
+            <div data-aos="fade-up"><SectionExclusiveOffer /></div>
+            <div data-aos="fade-up"><SectionExploreProducts /></div>
+            <div data-aos="fade-up"><SectionNewArrival /></div>
+            <div data-aos="fade-up"><Services /></div>
         </div>
     );
-}
+};
 
-export default HomePage
+export default HomePage;
