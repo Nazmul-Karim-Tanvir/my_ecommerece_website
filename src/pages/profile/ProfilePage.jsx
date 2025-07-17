3b5fb1d0d47ae2e0bbb439c712e171bc4bc07904
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2025-07-17 14:54:35 +0600

    some changes in style

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -74,14 +74,15 @@ const ProfilePage = () => {
             >
               My Offers
             </button>
+            <button
+              onClick={handleLogout}
+              className="w-full mt-6 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+            >
+              Logout
+            </button>
           </div>
           {/* Logout button at bottom of sidebar */}
-          <button
-            onClick={handleLogout}
-            className="w-full mt-6 bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-          >
-            Logout
-          </button>
+
         </div>
 
         {/* Content area */}
