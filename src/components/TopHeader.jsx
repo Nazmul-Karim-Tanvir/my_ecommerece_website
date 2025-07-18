aa25e96543c50ed5b6e224473a63832771ef3290
Nazmul Karim Tanvir <nazmulkarimtanvir@gmail.com>
2025-07-18 21:12:45 +0600

    added language feature for top header

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -1,35 +1,44 @@
-import React from 'react'
+import React from 'react';
 import { Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 const TopHeader = () => {
+    const { t, i18n } = useTranslation();
+
+    const handleLanguageChange = (e) => {
+        i18n.changeLanguage(e.target.value);
+    };
+
     return (
         <div className="w-full bg-black text-white text-md py-2 px-4">
             <div className="max-w-[1170px] mx-auto flex justify-between items-center">
 
                 {/* Center Part */}
                 <div className="flex-1 text-center">
-                    <span className="text-gray-200">
-                        Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
-                    </span>
+                    <span className="text-gray-200">{t('promo')}</span>
                     <Link
                         to="/product"
                         className="text-white font-semibold underline ml-2"
                         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
                     >
-                        ShopNow
+                        {t('shop_now')}
                     </Link>
                 </div>
 
                 {/* Right Part */}
                 <div className="ml-4">
-                    <select className="bg-black text-white">
-                        <option>English</option>
-                        <option>বাংলা</option>
+                    <select
+                        className="bg-black text-white"
+                        onChange={handleLanguageChange}
+                        value={i18n.language}
+                    >
+                        <option value="en">English</option>
+                        <option value="bn">বাংলা</option>
                     </select>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default TopHeader
+export default TopHeader;
